d8ee7cced30ba7d5504012344a9ff45169c152a5
mamisoa <mamisoa@gmail.com>
2024-10-20 00:10:16 +0200

    scaffold dashboard

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,13 +1,12 @@
-export function DashboardContent() {
-    return(
-        <>
-        <div>Dashboard</div>
-        </>
-    )
-}
+import { DashboardPage } from "./dashboard";
 
-export default function Dashboard() {
-    return(
-        <DashboardContent/>
-    )
-}
+export default async function Dashboard() {
+    return (
+      <div className="flex items-center justify-center min-h-screen bg-gray-100">
+        <div className="w-full max-w-4xl bg-white p-8 rounded-lg shadow-lg text-black">
+          <DashboardPage />
+        </div>
+      </div>
+    );
+  }
+  
